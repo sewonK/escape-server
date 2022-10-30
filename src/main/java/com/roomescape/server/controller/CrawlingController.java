224c997e16e146db57b5e565b86c40d075393381
SewonK <cutecat1128@naver.com>
2022-10-30 10:22:31 +0900

    [Feat] Crawling Controller - store 크롤링 구현

---
+++ src/main/java/com/roomescape/server/controller/CrawlingController.java
@@ -0,0 +1,37 @@
+package com.roomescape.server.controller;
+
+import com.roomescape.server.model.StoreDto;
+import com.roomescape.server.service.CrawlingService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/crawling")
+public class CrawlingController {
+    private static Logger logger = LoggerFactory.getLogger(CrawlingController.class);
+    private final CrawlingService crawlingService;
+
+    @GetMapping("/store")
+    public ResponseEntity<List<StoreDto>> getStore() {
+        return new ResponseEntity<>(crawlingService.getStore(), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/theme")
+    public void getTheme() {
+
+    }
+
+    @GetMapping("/reservation")
+    public void getReservation() {
+
+    }
+}
