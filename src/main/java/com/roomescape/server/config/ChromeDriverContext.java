aa50e61683a6435eb470476ae027bc2c41eace40
SewonK <cutecat1128@naver.com>
2022-10-30 10:18:41 +0900

    [Feat] Chrome driver Singleton 환경으로 변경(Bean 생성)

--- src/main/java/com/roomescape/server/config/ChromeDriverContext.java
+++ src/main/java/com/roomescape/server/config/ChromeDriverContext.java
@@ -7,34 +7,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ChromeDriverContext {
-    public static final String CHROME_DRIVER_ID = "webdriver.chrome.driver";
     private static final Logger logger = LoggerFactory.getLogger(ChromeDriverContext.class);
+    private static final String CHROME_DRIVER_ID = "webdriver.chrome.driver";
     private static final String CHROME_DRIVER_PATH = "C:/Users/Sewon/IdeaProjects/escape-server/chromedriver.exe";
-    private WebDriver driver;
 
     @Bean
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    @Bean
-    public WebDriver setupChromeDriver() {
+    public WebDriver chromeDriver() {
+        logger.info("@ChromeDriverContext: chromeDriver start");
         System.setProperty(CHROME_DRIVER_ID, CHROME_DRIVER_PATH);
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--start-maximized");
         options.addArguments("--blink-settings=imagesEnabled=false");
         options.setPageLoadStrategy(PageLoadStrategy.NONE);
-
-        try {
-            driver = new ChromeDriver(options);
-        } catch (Exception e) {
-            logger.error("[chrome driver error] msg: {}, cause: {}", e.getMessage(), e.getCause());
-        }
-
-        return driver;
+        return new ChromeDriver(options);
     }
 }
