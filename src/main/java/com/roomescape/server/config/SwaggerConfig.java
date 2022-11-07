449cd9ae961bab7f04cf1ee090159c44d1d78023
sewonK <cutecat1128@naver.com>
2022-11-07 21:39:36 +0900

    [Doc] Swagger UI Dependency 추가

---
+++ src/main/java/com/roomescape/server/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.roomescape.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.roomescape.server.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("방탈출 예약 정보 조회 서비스")
+                .description("* 현재는 키이스케이프 방탈출 정보만 제공")
+                .version("1.0")
+                .build();
+    }
+
+}
